7681f4a920f4f243ac202d98ceb58f5e5f30bee8
Sisi <dqgsh1055@gmail.com>
2019-11-26 21:36:08 +0900

    user model

--- core/models.py
+++ core/models.py
@@ -16,3 +16,14 @@ class UserManager(BaseUserManager):
         return user
 #     원래 create_user > _create_user 실행되는데,
 #     username 안쓰려고 여기서 한꺼번에 구현했다
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
