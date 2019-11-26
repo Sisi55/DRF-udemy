21af13c066db723f2d1e0335c7b11fead4b7a20e
Sisi <dqgsh1055@gmail.com>
2019-11-26 21:47:36 +0900

    test user model # assertRaise

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -14,3 +14,13 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@sisi.com'
+        user = get_user_model().objects.create_user(email, "test123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
