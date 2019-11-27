4862ce0c7212af747879e3e4dd23045e3b6e2376
Sisi <dqgsh1055@gmail.com>
2019-11-27 17:26:24 +0900

    image save test ok # [이전 커밋과 비교하며 살펴보기. +문서]

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
+import uuid
+import os
+
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import (
@@ -5,7 +8,13 @@ from django.contrib.auth.models import (
 )
 
 
-# Create your models here.
+def recipe_image_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
+
+
 class UserManager(BaseUserManager):
     # BaseUserManager.create_user()
 
@@ -78,6 +87,7 @@ class Recipe(models.Model):
     )
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
